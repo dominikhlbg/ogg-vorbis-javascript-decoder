c0c374c5d89ebdbaf06fee25b89316d8894eb06b
Dominik Homberger <dominikhlbg@gmail.com>
2017-04-26 20:58:26 +0200

    License & Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 OGG Vorbis JavaScript decoder
 =============================
-Here you can get the hand ported source code of std_vorbis.c into javascript. Source code is from 2012.
+Here you can get my hand ported source code from stb_vorbis.c (Sean Barrett) to javascript (Dominik Homberger). Source code is from 2012.
 
 Here you can see an example:
 http://libwebpjs.hohenlimburg.org/vp8/ogg-vorbis-javascript-decoder/
--- oggvorbis.js
+++ oggvorbis.js
@@ -1,42 +1,12 @@
+// Ogg Vorbis I audio decoder  -- version 0.99996
+//
+// Written in April 2007 by Sean Barrett, sponsored by RAD Game Tools. http://nothings.org/stb_vorbis/
+//
+// This Version is a javascript version hand ported by Dominik Homberger (dominikhlbg@gmail.com)
+// License: CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+
 (function(){
-
-var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-function base64encode(str) {
-    var out, i, len;
-    var c1, c2, c3;
-
-    len = str.length;
-    i = 0;
-    out = "";
-    while(i < len) {
-	c1 = str.charCodeAt(i++) & 0xff;
-	if(i == len)
-	{
-	    out += base64EncodeChars.charAt(c1 >> 2);
-	    out += base64EncodeChars.charAt((c1 & 0x3) << 4);
-	    out += "==";
-	    break;
-	}
-	c2 = str.charCodeAt(i++);
-	if(i == len)
-	{
-	    out += base64EncodeChars.charAt(c1 >> 2);
-	    out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-	    out += base64EncodeChars.charAt((c2 & 0xF) << 2);
-	    out += "=";
-	    break;
-	}
-	c3 = str.charCodeAt(i++);
-	out += base64EncodeChars.charAt(c1 >> 2);
-	out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
-	out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
-	out += base64EncodeChars.charAt(c3 & 0x3F);
-    }
-    return out;
-}
-
-if (!window.btoa) window.btoa = base64encode;
 
 var buffer_time;
 var player_type_audio;
